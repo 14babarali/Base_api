6fa7789425b4d2741f47da2a36b65620c814aad5
14babarali <12babaraz@gmail.com>
2024-08-17 06:18:28 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bookRoutes = require('./routes/bookRoutes.js'); // Import routes
+const app = express();
+const URI = process.env.MONGODB_URI;
+
+mongoose.connect(URI);
+const database = mongoose.connection;
+
+database.on('error', (err) => {
+    console.log(err);
+});
+
+database.once('connected', () => {
+    console.log('Database Connected to the server');
+});
+
+app.use(express.json());
+app.use('/api', bookRoutes); // Use the routes
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Books API!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
